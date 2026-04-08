ce745f3a323e1fc877a09f98b054c51f1e929cc5
KingBlack <vicente.marin013@gmail.com>
2026-04-08 05:04:11 +0200

    feat: implement automated web scraper for product reviews with site-specific configurations

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -29,6 +29,43 @@ except ImportError:
 # CONFIGURACIÓN DEL SITIO
 
 SITE_CONFIGS = {
+    "pccomponentes.com": {
+        "scroll_speed": 1.2,
+        "cookies_btn":    (By.XPATH,      "//button[contains(., 'Aceptar')]"),
+        "bloque":         (By.CSS_SELECTOR, "[class*='commentDataContainer']"),
+        "paginacion": {
+            "tipo":        "boton",
+            "selector":    (By.XPATH, "//button[contains(., 'Cargar más opiniones')]"),
+            "max_paginas": None,
+        },
+        "campos": {
+            "rating": {
+                "selector": (By.CSS_SELECTOR, "[data-testid='rating-bar-percent']"),
+                "tipo": "rating_style",
+            },
+            "fecha": {
+                "selector": (By.CSS_SELECTOR, "[class*='captionRegular']"),
+                "tipo": "text",
+            },
+            "comentario": {
+                "selector": (By.CSS_SELECTOR, "[class*='body2Regular']"),
+                "tipo": "text",
+            },
+            "pros": {
+                "selector": (By.CSS_SELECTOR, "[data-testid='pros'] li"),
+                "tipo": "lista",
+            },
+            "contras": {
+                "selector": (By.CSS_SELECTOR, "[data-testid='cons'] li"),
+                "tipo": "lista",
+            },
+        },
+        "url_mod": {
+            "tipo": "insertar",
+            "ancla": "pccomponentes.com/",
+            "nuevo": "opiniones/",
+        }
+    },
     "mediamarkt.es": {
         "scroll_speed": 3,
         "cookies_btn": (By.XPATH,       "//button[contains(., 'Aceptar')]"),
@@ -128,61 +165,89 @@ SITE_CONFIGS = {
             "tipo": "text",
         },
     },
-    "filtros": None
-    },
+    "filtros": None,
+    "url_mod": {
+        "tipo": "reemplazar",
+        "ancla": "/p/",
+        "nuevo": "/r/"
+    }
+    }
+
 }
 
 prefs = {
     "profile.managed_default_content_settings.images": 2,
-    "profile.managed_default_content_settings.fonts": 2
+    "profile.managed_default_content_settings.fonts": 2,
+    "profile.default_content_setting_values.notifications": 2
 }
 
+def optimizar_url_segun_config(url, config):
+    mod = config.get("url_mod")
+    
+    if not mod:
+        return url
+
+    tipo = mod.get("tipo")
+    ancla = mod.get("ancla")
+    nuevo = mod.get("nuevo")
+
+    if tipo == "insertar":
+        # Divide en el ancla e inserta justo después
+        if ancla in url:
+            partes = url.split(ancla, 1)
+            suffix = mod.get("suffix", "")
+            return f"{partes[0]}{ancla}{nuevo}{partes[1]}{suffix}"
+            
+    elif tipo == "reemplazar":
+        # Sustituye la primera aparición del ancla por el nuevo valor
+        print("reemplazar")
+        return url.replace(ancla, nuevo, 1)
+
+    return url
 
 def scrape_opiniones(url):
-    url = su.normalizar_url(url)
     domain = su.get_domain(url)
     config = SITE_CONFIGS.get(domain)
 
     if not config:
         raise ValueError(f"No hay configuración para: {domain}")
 
+
+    url_final = optimizar_url_segun_config(url, config)
+
     options = uc.ChromeOptions()
+
+    options.add_experimental_option("prefs", prefs)  
 
     # Iniciar driver
     
-    #options.add_argument("--headless=new") # TODO COMENTARLO Y MANEJARLO CORRECTAMENTE AL TENER LAS PÁGINAS PULIDAS
+    # options.add_argument("--headless=new")
     options.add_argument("--window-size=1920,1080")
 
-    #options.add_argument("--disable-gpu")
 
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    options.add_experimental_option("prefs", prefs)
-
     driver = uc.Chrome(options=options, version_main=145)
 
+    wait = WebDriverWait(driver, 10) # Centralizamos el wait
+    
     try: 
 
-        driver.get(url)
-        time.sleep(1.8)
+        driver.get(url_final)
 
         # Cerrar cookies
         try:
             by, sel = config["cookies_btn"]
-            WebDriverWait(driver, 6).until(EC.element_to_be_clickable((by, sel))).click()
+            wait.until(EC.element_to_be_clickable((by, sel))).click()
             print("✅ Cookies aceptadas")
             time.sleep(0.3)
         except:
             print("ℹ️ No hay banner de cookies")
 
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight * 0.65);")
-        time.sleep(1.8)
-        
-        bloques = driver.find_elements(By.CSS_SELECTOR, "[data-test='single-review-card']")
-        print(f"🔍 Bloques encontrados: {len(bloques)}")
 
-        WebDriverWait(driver, 14).until(
+        wait.until(
             EC.presence_of_element_located(config["bloque"])
         )
 
@@ -221,7 +286,8 @@ if __name__ == "__main__":
     urlmm = "https://www.mediamarkt.es/es/product/_apple-iphone-17-azul-neblina-256-gb-5g-63-oled-super-retina-xdr-chip-a19-ios-1606127.html"
     urlax = "https://es.aliexpress.com/item/1005005952420757.html?spm=a2g0o.best.0.0.77b922aeMkiNt7&pdp_npi=6%40dis%21EUR%214%2C61%E2%82%AC%210%2C99%E2%82%AC%21%21%21%21%21%402103892f17736749760602052e01ac%2112000035000006810%21btfaff%21%21%21%211%210%21&afTraceInfo=1005005952420757__pc__pcBestMore2Love__oU6Kj8D__1773674976369&gatewayAdapt=glo2esp#nav-review"
     urldec = "https://www.decathlon.es/es/p/zapatillas-running-adidas-runblaze-hombre-negro/361354/c1m8929086"
-    
+    urlpcc = "https://www.pccomponentes.com/tarjeta-grafica-asus-prime-geforce-rtx-5060-oc-edition-8gb-gddr7-reflex-2-rtx-ai-dlss4"
+
     resultados = scrape_opiniones(urldec)
 
     print(resultados)
